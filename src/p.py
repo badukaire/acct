2f0af1d2fba3371e4ef79ac12db3cb673179ad37
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2021-01-24 23:32:02 +0100

    indent balance like other outputs

--- src/p.py
+++ src/p.py
@@ -265,7 +265,7 @@ class Acct :
     for lsAcct in lListKeys2 :
       # no need to try:
       lfSaldo = self.mDictSaldo[ lsAcct ]
-      print( "%-12s: %9.2f" % ( lsAcct, lfSaldo ) )
+      print( "%-12s : %9.2f" % ( lsAcct, lfSaldo ) )
       # TODO : use Acct.W_ACCT for formatting
 
       lsAcctType = lsAcct[ 0 : 2 ]
